fc26d4dba8bfbce7b6ba4848ead2e52cc251d729
Larissa Ramos <devlarissasr@gmail.com>
2022-10-23 19:30:57 -0300

    feat: first step

--- src/pages/Step1/index.jsx
+++ src/pages/Step1/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./styles.css";
 import ChatBallon from "../../components/ChatBalloon";
+import PandaHello from "../../assets/panda/panda-hello.png";
+import { Button } from "@react-bulk/web";
 
 export default function Step1() {
   return (
     <div className="step-body">
-    <ChatBallon />
+      <ChatBallon />
+      <div className="container-img">
+        <img src={PandaHello} alt="Panda Hello" className="img-step1" />
+        <Button>Next</Button>
+        <Button>Back</Button>
+      </div>
     </div>
   );
 }
