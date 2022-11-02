bee7b3a0e1740fd7335af05ca5ec2d2cb7459f3b
Larissa Ramos <devlarissasr@gmail.com>
2022-11-01 21:09:48 -0300

    fix card step 2

--- src/pages/Step2/index.jsx
+++ src/pages/Step2/index.jsx
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
-import ChatBallon from "../../components/ChatBalloon";
 import PandaHello from "../../assets/panda/panda-hello.png";
-import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export default function Step2() {
   return (
     <div className="step-body">
       <div className="container-img">
+      <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
         <img src={PandaHello} alt="Panda Hello" className="img-step1" />
       </div>
+     
     </div>
   );
 }
