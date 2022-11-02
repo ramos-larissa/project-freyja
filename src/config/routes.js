a41ea2000e6bf73ba7c898197d962e43dc8d5c76
thiagodaurizio <thiago_daurizio@hotmail.com>
2022-11-02 11:16:46 -0300

    feat: error page implanted

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 import "../index.css";
 
+import Error from "../pages/Error";
 import Home from "../pages/Main";
 import Step1 from "../pages/Step1";
 import Step2 from "../pages/Step2";
@@ -13,6 +14,7 @@ function RoutesConfig() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/step1" element={<Step1 />} />
         <Route exact path="/step2" element={<Step2 />} />
+        <Route exact path="/error" element={<Error/>} />
       </Switch>
     </BrowserRouter>
   );
