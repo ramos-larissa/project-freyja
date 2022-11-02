c9db3c01dc4f08e7abb766135fcd808f1473a44a
Larissa Ramos <devlarissasr@gmail.com>
2022-11-02 10:52:37 -0300

    feat: adding runes

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,6 +5,7 @@ import "../index.css";
 import Home from "../pages/Main";
 import Step1 from "../pages/Step1";
 import Step2 from "../pages/Step2";
+import Step3 from "../pages/Step3";
 
 function RoutesConfig() {
   return (
@@ -13,6 +14,7 @@ function RoutesConfig() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/step1" element={<Step1 />} />
         <Route exact path="/step2" element={<Step2 />} />
+        <Route exact path="/step3" element={<Step3 />} />
       </Switch>
     </BrowserRouter>
   );
